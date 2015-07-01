ec0d3455c9682f0529890b1c8442945f97f3d7d0
Daniel Strauss <daniel@oomnitza.com>
2015-07-01 10:53:46 -0700

    minor fix to logging paths when not running as binary.

--- utils/relative_path.py
+++ utils/relative_path.py
@@ -1,17 +1,25 @@
 import os
 import sys
 
+"""
+If we are running in a |PyInstaller| bundle, PyInstaller adds the attributes 'frozen' and '_MEIPASS' to the sys.
+'frozen': It's used to learn the app is running "live" (from source) or "frozen" (part of bundle)
+'_MEIPASS': It contains the path to the folder containing your script and any other files or folders bundled with it
+"""
 
 def relative_path(filename):
     """
-    Dynamic file path depends on running in a PyInstaller bundle
-    or normal Python environment
+    This is used to convert the path of the bundle data file with binary.
+    :return
     """
     return os.path.join(getattr(sys, '_MEIPASS', os.path.abspath(".")), filename)
 
 
 def relative_app_path(filename):
-    # determine if application is a script file or frozen exe
+    """
+    This is used to convert the path of the file in directory connector.
+    :return
+    """
     if getattr(sys, 'frozen', False):
         application_path = os.path.dirname(sys.executable)
     else:
